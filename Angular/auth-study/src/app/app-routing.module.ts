e57806329dbf4378a79604634bfa78d9590ca699
DangQuangSE <quangndse180246@fpt.edu.vn>
2025-09-27 16:53:18 +0700

    fix router

--- Angular/auth-study/src/app/app-routing.module.ts
+++ Angular/auth-study/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [{ path: 'singup', component: SignupComponent }];
+const routes: Routes = [{ path: 'signup', component: SignupComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
